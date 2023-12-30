8142643fd134a6bece5277662bbd18dd4bd29463
herbuś <wujowujo3@gmail.com>
2023-12-30 01:03:14 +0100

    dev: informations displayed with markdown formatting

--- screens/creation_screen/tabbed_content_tabs/race_tab_contents/RaceTab.py
+++ screens/creation_screen/tabbed_content_tabs/race_tab_contents/RaceTab.py
@@ -1,6 +1,6 @@
 from textual import on
 from textual.app import ComposeResult
-from textual.widgets import Label, TabPane, Select, Footer
+from textual.widgets import Label, TabPane, Select, Footer, Markdown
 from textual.widgets._select import NoSelection
 from textual.containers import Center
 
@@ -19,6 +19,16 @@ RACES = [
     ("Tiefling", "Tiefling"),
 ]
 
+INTRODUCTION = """
+Welcome, brave adventurer, to a world of limitless possibilities, a realm where power is yours for the taking, and destiny is in your hands. Your journey begins here, and the first step is to choose your class.
+
+Each class is unique, offering its own set of skills, abilities, and traits that will shape your adventure. Will you be a valiant knight, wielding your sword with honor and courage? Or perhaps a cunning rogue, using stealth and trickery to overcome your foes? Maybe you see yourself as a wise wizard, harnessing the arcane forces to bend reality to your will?
+
+The choice is yours, and each decision will pave the way for your unique story. Remember, there is no right or wrong choice, only different paths on the road to glory. So, take a moment, breathe deep, and listen to your heart. Who do you want to be in this grand adventure?
+
+Choose wisely, for the path you set upon will define your journey, your challenges, and your ultimate destiny. Good luck, adventurer, and may fortune favor the bold!
+"""
+
 class RaceTabPane(TabPane):
     def __init__(
         self, 
@@ -29,13 +39,13 @@ class RaceTabPane(TabPane):
     ):
         super().__init__(title, id=id, **kwargs)
         self.bottombar = bottombar
-        self.current_race = Label("Please select your race before you continue.")
+        self.current_race = Markdown("# Please select your race before you continue.", classes="selected_race_title")
         self.completion_status = None
         self.race_info = RaceTabInformations()
 
     def compose(self) -> ComposeResult:
-        with Center():
-            yield self.current_race
+        yield self.current_race
+        yield Markdown(INTRODUCTION, classes="race_introduction")
         yield Select(prompt="Select a race", options=RACES, name="race_select", classes="race_select")
         yield self.race_info
         
@@ -43,13 +53,13 @@ class RaceTabPane(TabPane):
     def on_select_changed(self, event: Select.Changed) -> None:
         selected_race = event.value 
         if isinstance(selected_race, NoSelection):
-            self.current_race.update("Please select your race.")
+            self.current_race.update("# Please select your race.")
             self.race_info.update_race(selected_race)
             if self.completion_status is not None:
                 self.bottombar.increase_progress(-1)
                 self.completion_status = None
         else:
-            self.current_race.update(f"Selected race: {selected_race}")
+            self.current_race.update(f"# Selected race: {selected_race}")
             self.race_info.update_race(selected_race)
             self.notify("You can now check all the important informations about it on the right. If you believe that's the race you wanna go with, you can now continue and take a look at the Class tab.", title=f"Successfully selected {selected_race} race.", timeout=5)
             if self.completion_status is None:
